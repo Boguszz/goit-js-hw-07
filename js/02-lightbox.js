580fb27eae0b6d6e943fe9b37e8c40d679c3ce36
Bohdan Strilets <bohdan.strilets@gmail.com>
2021-11-26 17:02:57 +0100

    gallery 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryRefs = document.querySelector(".gallery");
+
+(function createMarkup() {
+  const itemMarkup = galleryItems
+    .map(({ original, preview, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    `;
+    })
+    .join("");
+  galleryRefs.insertAdjacentHTML("beforeend", itemMarkup);
+})();
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
